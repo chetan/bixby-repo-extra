b71538246ff9d6a2f961282a7c1eb30776af8bd7
Chetan Sarva <chetan@pixelcop.net>
2014-11-26 12:59:21 -0500

    change uid to root so we can properly read the sensor

--- dht_sensor/bin/monitoring/dht_sensor.rb
+++ dht_sensor/bin/monitoring/dht_sensor.rb
@@ -14,15 +14,37 @@ module Bixby
         pin  = @options["pin"] || 4
         type = @options["sensor_type"] || 22
 
-        begin
-          reading = ::DhtSensor.read(pin, type)
-        rescue Exception => ex
-          return error(ex)
+        # fork a subprocess and change uid to 0 (root) so we can read from /dev/mem
+        rd, wr = IO.pipe
+        pid = fork do
+          rd.close
+          Process.uid = 0
+          Process.euid = 0
+          begin
+            reading = ::DhtSensor.read(pin, type)
+            hash = {"temperature" => reading.temp_f, "humidity" => reading.humidity}
+          rescue Exception => ex
+            hash = {"error" => ex.message + "\n" + ex.backtrace.join("\n")}
+          end
+          wr.write(MultiJson.dump(hash))
+          wr.close
+        end
+        wr.close
+        ret = rd.read
+        Process.wait(pid)
+
+        if ret.nil? or ret.empty? then
+          return error("failed to read from sensor")
+        end
+
+        json = MultiJson.load(ret)
+        if json["error"] then
+          return error(json["error"])
         end
 
         add_metric({
-          :temperature => reading.temp_f,
-          :humidity    => reading.humidity
+          :temperature => json["temperature"],
+          :humidity    => json["humidity"]
         })
 
       end
